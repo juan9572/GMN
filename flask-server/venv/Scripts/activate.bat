101eaa96c77f775ba4fda819d3aa9e6fdefcc97c
Julian Gomez <julixd41@gmail.com>
2022-07-31 21:25:27 -0500

    aaaaa

--- flask-server/venv/Scripts/activate.bat
+++ flask-server/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Desktop\Universidad\Sexto Semestre\Análisis Numérico\Trabajo final\Código\flask-server\venv
+set VIRTUAL_ENV=C:\Users\anyth\Documents\Universidad\6semestre\Analisis\Trabajo final\GMN\flask-server\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
